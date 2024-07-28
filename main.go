83099533a2c339b722c81b780530c2e8b9db61f3
Ghostpappi <tonyeakulasah@gmail.com>
2024-07-28 01:17:18 +0100

    added

--- main.go
+++ main.go
@@ -1,35 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	ConferenceName := "HNG Conference"
 	const ConferenceTickets = 50
 	var RemainingTickets uint = 50
+	var bookings []string
 
 	fmt.Printf("Welcome to %v booking application.\nWe have total of %v Tickets and %v are still available.\nGet your tickets here to attend\n", ConferenceName, ConferenceTickets, RemainingTickets)
 
-	var FirstName string
-	var LastName string
-	var Email string
-	var userTickets uint
+	for {
 
-	//	Asking users for their name
-	fmt.Println("Enter your First Name:")
-	fmt.Scan(&FirstName)
+		var FirstName string
+		var LastName string
+		var Email string
+		var userTickets uint
 
-	fmt.Println("Enter your Last Name:")
-	fmt.Scan(&LastName)
+		//	Asking users for their name
+		fmt.Println("Enter your First Name:")
+		fmt.Scan(&FirstName)
 
-	fmt.Println("Enter number of Tickets:")
-	fmt.Scan(&userTickets)
+		fmt.Println("Enter your Last Name:")
+		fmt.Scan(&LastName)
 
-	fmt.Println("Enter your Email Address:")
-	fmt.Scan(&Email)
+		fmt.Println("Enter number of Tickets:")
+		fmt.Scan(&userTickets)
 
-	RemainingTickets = RemainingTickets - userTickets
+		fmt.Println("Enter your Email Address:")
+		fmt.Scan(&Email)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. you will recive a confirmation email at %v\n", FirstName, LastName, userTickets, Email)
+		RemainingTickets = RemainingTickets - userTickets
+		bookings = append(bookings, FirstName+" "+LastName)
 
-	fmt.Printf("%v Tickets Remaining for %v\n", RemainingTickets, ConferenceName)
+		fmt.Printf("Thank you %v %v for booking %v tickets. you will recive a confirmation email at %v\n", FirstName, LastName, userTickets, Email)
+
+		fmt.Printf("%v Tickets Remaining for %v\n", RemainingTickets, ConferenceName)
+
+		var firstNames []string
+		for _, booking := range bookings {
+			names := strings.Fields(booking)
+
+			firstNames = append(firstNames, names[0])
+		}
+
+		fmt.Printf("The first name of bookings are: %v\n", firstNames)
+
+	}
+
 }
